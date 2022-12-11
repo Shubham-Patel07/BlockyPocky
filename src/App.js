b6729df8b2884fe8eb5297377aca9aa12a0afdf4
rseragon <rseragon702@gmail.com>
2022-12-11 07:22:25 +0530

    Add: get ethereum accounts

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react';
+import Web3 from 'web3';
+import './App.css';
+import {CONTRACT_ABI, CONTRACT_ADDRESS} from './consts'
+
+const web3 = new Web3(window.web3.currentProvider);
+/* var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545')); */
+/* const web3 = new Web3("http://127.0.0.1:7545"); */
+
+function App() {
+
+	// Enable metamask shit
+	window.ethereum.enable();
+
+	const [text, settext] = useState(" ")
+	const captureText = (event) => {
+		event.preventDefault();
+		const uploadtext = text;
+
+
+		/* window.ethereum.request({ method: 'eth_requestAccounts' }).then(acc => { */
+		web3.eth.getAccounts().then(acc => {
+			console.log(acc);
+		if(!acc) {
+				window.alert("failed to get account");
+				return;
+			}
+			const contract = new web3.eth.Contract(CONTRACT_ABI['abi'], CONTRACT_ADDRESS);
+
+			contract.deploy({data: CONTRACT_ABI['bytecode']})
+				.send({from: acc[0]})
+				.on('receipt', (receipt) => {
+		            console.log("Contract Address:", receipt.contractAddress);
+				})
+				.then((initialContract) => {
+					initialContract.methods.textEdit("inputhash", "prevHash", 5005, "ipfshash", acc[0]).call((err, data) => {
+						                console.log("Initial Data:", data);
+					});
+
+			});
+		});
+
+
+
+
+	};
+
+	return (
+		<div className="App">
+			<nav className='sticky-section'>
+				<h1>Blockchain Pastebin</h1>
+				<button className="btn" onClick={captureText}>Login</button>
+			</nav>
+			<div class="container">
+				<textarea id="page_form_content" placeholder="Paste Here!" rows="4 " cols="50" onChange ={(event)=>{setText(event.target.value)}} > </textarea>
+			</div>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/consts.js
@@ -0,0 +1,15 @@
+export const CONTRACT_ABI = require('./abi/TextUpload.json')
+
+export const CONTRACT_ADDRESS = "0xbAe1c0D33A6e220C276E834DB5027315AD12eB7B";
+
+/*
+ * Metadata and sources of "textupload" were published successfully. 
+
+contracts/textUpload.sol : 
+
+bzz-raw://86e0608f4a7874655718d351961929e73df512174fd86e7c29267ad465841c2d
+
+metadata.json : 
+
+bzz-raw://d59ee85a9a6b737fa936a0a999a45e1e31f1de8f9e5251e749d8e77c699cfd1d
+*/
